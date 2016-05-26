a72e31dd1cd2b779b2259abfea70c3e0d3cd8dca
Nadeem Abdulla <nadeemabd@gmail.com>
2016-05-26 21:08:49 +0530

    Support for left and right sidebars

---
+++ sidebar-left.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package nadtheme
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary-left" class="widget-area sidebar-left" role="complementary">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
