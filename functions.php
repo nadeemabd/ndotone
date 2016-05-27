d4a196095266229c09e14a5f09b9f2a44a50853c
Nadeem Abdulla <nadeemabd@gmail.com>
2016-05-28 00:32:22 +0530

    Remove redundant navigation.js file
    function.js file has same functionality

--- functions.php
+++ functions.php
@@ -149,7 +149,7 @@ function nadtheme_scripts()
 {
     wp_enqueue_style('nadtheme-style', get_stylesheet_uri());
 
-    wp_enqueue_script('nadtheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true);
+//    wp_enqueue_script('nadtheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true);
 
 
     wp_enqueue_script('nadtheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true);
