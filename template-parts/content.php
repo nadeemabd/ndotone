e6235d0d5079c0f7d4c0656e48be2377a4a8b2f6
Nadeem Abdulla <nadeemabd@gmail.com>
2016-06-01 16:17:43 +0530

    Add post thumbnail and sticky tag

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,7 +10,16 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<a class="post-thumbnail" href="<?php the_permalink(); ?>">
+		<?php the_post_thumbnail('medium'); ?>
+	</a>
+
 	<header class="entry-header">
+		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+			<span class="sticky-post"><?php _e( 'Featured', 'nadtheme' ); ?></span>
+		<?php endif; ?>
+
 		<?php
 			if ( is_single() ) {
 				the_title( '<h1 class="entry-title">', '</h1>' );
@@ -28,22 +37,22 @@
 
 	<div class="entry-content">
 		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'nadtheme' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+//			the_content( sprintf(
+//				/* translators: %s: Name of current post. */
+//				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'nadtheme' ), array( 'span' => array( 'class' => array() ) ) ),
+//				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+//			) );
+//
+//			wp_link_pages( array(
+//				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'nadtheme' ),
+//				'after'  => '</div>',
+//			) );
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'nadtheme' ),
-				'after'  => '</div>',
-			) );
-
-//		the_excerpt();
+		the_excerpt();
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php nadtheme_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+<!--	<footer class="entry-footer">-->
+<!--		--><?php //nadtheme_entry_footer(); ?>
+<!--	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
