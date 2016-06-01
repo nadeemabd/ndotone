981e41e2848f84b4ab0a04f5f2b76e1525025cd6
Nadeem Abdulla <nadeemabd@gmail.com>
2016-06-01 16:18:32 +0530

    Remove featured image

--- header.php
+++ header.php
@@ -25,14 +25,19 @@
     <a class="skip-link screen-reader-text" href="#main"><?php esc_html_e('Skip to content', 'nadtheme'); ?></a>
 
     <header id="masthead" class="site-header" role="banner" style="background-image: url('<?php get_custom_header_image(); ?>')">
+        <?php if (!is_front_page() && !is_home()) : ?>
+        <p class="side-title">
+            <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
+        </p>
+        <?php endif; ?>
 
         <nav id="site-navigation" class="main-navigation" role="navigation">
 
-            <?php if (!is_front_page() && !is_home()) : ?>
-                <p class="site-title">
-                    <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
-                </p>
-            <?php endif ?>
+<!--            --><?php //if (!is_front_page() && !is_home()) : ?>
+<!--                <p class="site-title">-->
+<!--                    <a class="nav-site-title" href="--><?php //echo esc_url(home_url('/')); ?><!--" rel="home">--><?php //bloginfo('name'); ?><!--</a>-->
+<!--                </p>-->
+<!--            --><?php //endif ?>
 
             <div class="site-header-menu">
             <?php if (has_nav_menu('primary')): ?>
@@ -42,20 +47,35 @@
             </div>
         </nav><!-- #site-navigation -->
 
-        <div class="site-branding">
 
-            <?php if (is_front_page() && is_home()) : ?>
+            <?php if ( !is_single() && !is_page() && !is_archive() ) : ?>
+                <div class="site-branding">
+
                 <h1 class="site-title">
-                    <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-            <?php else : ?>
-                <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-            <?php endif;
-            $description = get_bloginfo('description', 'display');
-            if ($description || is_customize_preview()) : ?>
-                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+                    <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
+<!--                    --><?php //bloginfo('name'); ?>
+                </h1>
+                <?php $description = get_bloginfo('description', 'display');
+                if ($description || is_customize_preview()) : ?>
+                    <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+                <?php endif; ?>
+<!--            --><?php //elseif (is_single()) : ?>
+<!--                <h2 class="site-title">-->
+<!--                    <a href="--><?php //echo esc_url(home_url('/')); ?><!--" rel="home">--><?php //bloginfo('name'); ?><!--</a>-->
+<!--                </h2>-->
+<!--                --><?php //the_title('<h1 class="entry-title">', '</h1>'); ?>
+<!--                --><?php //nadtheme_get_the_categories() ?>
+<!--            --><?php //elseif (is_archive()) : ?>
+<!--                <div class="page-header">-->
+<!--                    --><?php
+//                    the_archive_title( '<h1 class="page-title">', '</h1>' );
+//                    the_archive_description( '<div class="taxonomy-description">', '</div>' );
+//                    ?>
+<!--                </div><!-- .page-header -->
+                </div><!-- .site-branding -->
+
             <?php endif; ?>
 
-        </div><!-- .site-branding -->
 
     </header><!-- #masthead -->
 
