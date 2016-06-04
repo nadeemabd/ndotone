961c7d2f51bf53709c0a53ef22faf650ef255576
Nadeem Abdulla <nadeemabd@gmail.com>
2016-06-05 00:31:43 +0530

    Add custom post navigation

--- single.php
+++ single.php
@@ -17,7 +17,24 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', 'single' );
 
-			the_post_navigation();
+//			the_post_navigation();
+
+			if (is_singular('attachment')) {
+				// Parent post navigation.
+				the_post_navigation(array(
+						'prev_text' => _x('<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'nadtheme'),
+				));
+			} elseif (is_singular('post')) {
+				// Previous/next post navigation.
+				the_post_navigation(array(
+						'next_text' => '<span class="post-title">%title</span>' .
+								'<span class="meta-nav" aria-hidden="true">' . __('Next', 'nadtheme') . '</span>' .
+								'<span class="screen-reader-text">' . __('Next post:', 'nadtheme') . '</span>',
+						'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __('Previous', 'nadtheme') . '</span>' .
+								'<span class="screen-reader-text">' . __('Previous post:', 'nadtheme') . '</span>' .
+								'<span class="post-title">%title</span>',
+				));
+			}
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
