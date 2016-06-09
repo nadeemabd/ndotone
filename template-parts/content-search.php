690b6fc8fc23c9392c76158562f48904e7cdcb9e
Nadeem Abdulla <nadeemabd@gmail.com>
2016-06-09 16:18:02 +0530

    Markup changes to content

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,21 +10,28 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	<?php if(has_post_thumbnail()) : ?>
+		<div class="post-thumbnail" style="background-image: url('<?php echo the_post_thumbnail_url() ?>')">
+			<a class="cover-link" href="<?php the_permalink(); ?>"></a>
+		</div>
+	<?php endif; ?>
+	<div class="post-content">
+		<header class="entry-header">
+			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php nadtheme_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+	<!--		--><?php //if ( 'post' === get_post_type() ) : ?>
+	<!--		<div class="entry-meta">-->
+	<!--			--><?php //nadtheme_posted_on(); ?>
+	<!--		</div>--><!-- .entry-meta -->
+	<!--		--><?php //endif; ?>
+		</header><!-- .entry-header -->
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+		<div class="entry-content">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+	</div>
 
-	<footer class="entry-footer">
-		<?php nadtheme_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+<!--	<footer class="entry-footer">-->
+<!--		--><?php //nadtheme_entry_footer(); ?>
+<!--	</footer>--><!-- .entry-footer -->
 </article><!-- #post-## -->
