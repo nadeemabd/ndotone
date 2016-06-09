7785f42c216bda200c89cef6a6951fd85e49fe83
Nadeem Abdulla <nadeemabd@gmail.com>
2016-06-09 16:18:17 +0530

    Markup changes to content

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,47 +11,46 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<a class="post-thumbnail" href="<?php the_permalink(); ?>">
-		<?php the_post_thumbnail('medium'); ?>
-	</a>
+	<?php if(has_post_thumbnail()) : ?>
+		<div class="post-thumbnail" style="background-image: url('<?php echo the_post_thumbnail_url() ?>')">
+			<a class="cover-link" href="<?php the_permalink(); ?>"></a>
+		</div>
+	<?php endif; ?>
 
-	<header class="entry-header">
-		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
-			<span class="sticky-post"><?php _e( 'Featured', 'nadtheme' ); ?></span>
-		<?php endif; ?>
+<!--	--><?php //if(has_post_thumbnail()) : ?>
+<!--	<div class="post-thumbnail">-->
+<!--		--><?php //the_post_thumbnail(); ?>
+<!--		<a class="cover-link" href="--><?php //the_permalink(); ?><!--"></a>-->
+<!--	</div>-->
+<!--	--><?php //endif; ?>
 
-		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
+	<div class="post-content">
+		<header class="entry-header">
+			<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+				<span class="sticky-post"><?php _e( 'Featured', 'nadtheme' ); ?></span>
+			<?php endif; ?>
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php nadtheme_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+			<?php
+				if ( is_single() ) {
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				} else {
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				}
 
-	<div class="entry-content">
-		<?php
-//			the_content( sprintf(
-//				/* translators: %s: Name of current post. */
-//				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'nadtheme' ), array( 'span' => array( 'class' => array() ) ) ),
-//				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-//			) );
-//
-//			wp_link_pages( array(
-//				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'nadtheme' ),
-//				'after'  => '</div>',
-//			) );
+	//		if ( 'post' === get_post_type() ) : ?>
+	<!--		<div class="entry-meta">-->
+	<!--			--><?php //nadtheme_posted_on(); ?>
+	<!--		</div>--><!-- .entry-meta -->
+	<!--		--><?php
+	//		endif; ?>
+		</header><!-- .entry-header -->
 
-		the_excerpt();
-		?>
-	</div><!-- .entry-content -->
-
+		<div class="entry-content">
+			<?php
+			the_excerpt();
+			?>
+		</div><!-- .entry-content -->
+	</div>
 <!--	<footer class="entry-footer">-->
 <!--		--><?php //nadtheme_entry_footer(); ?>
 <!--	</footer><!-- .entry-footer -->
