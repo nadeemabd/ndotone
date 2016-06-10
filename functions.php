1b0ccdce9deae73586ae82b38fd08853dbe25708
Nadeem Abdulla <nadeemabd@gmail.com>
2016-06-10 18:59:51 +0530

    Add social and footer menu

--- functions.php
+++ functions.php
@@ -58,6 +58,8 @@ if (!function_exists('nadtheme_setup')) :
         // This theme uses wp_nav_menu() in one location.
         register_nav_menus(array(
             'primary' => esc_html__('Primary', 'nadtheme'),
+            'social' => esc_html__('Social', 'nadtheme'),
+            'footer' => esc_html__('Footer', 'nadtheme'),
         ));
 
         /*
