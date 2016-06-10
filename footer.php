6b8e14cf3eafe9ce44d4315809a7cfa8ab2d7911
Nadeem Abdulla <nadeemabd@gmail.com>
2016-06-10 18:59:37 +0530

    Footer markup

--- footer.php
+++ footer.php
@@ -11,15 +11,45 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nadtheme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'nadtheme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'nadtheme' ), 'nadtheme', '<a href="http://nadeemabd.github.io/" rel="designer">Nadeem Abdulla</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+
+    <div class="site-info">
+        <a href="<?php echo esc_url(__('https://wordpress.org/', 'nadtheme')); ?>"><?php printf(esc_html__('%s', 'nadtheme'), 'WordPress'); ?></a>
+        <span class="sep"> | </span>
+        <?php printf(esc_html__('%1$s by %2$s', 'nadtheme'), 'nadtheme', '<a href="http://nadeemabd.github.io/" rel="designer">Nadeem Abdulla</a>'); ?>
+    </div><!-- .site-info -->
+
+    <div class="site-copyright">
+        <div class="copy">
+            <?php _e('Copyright', 'nadtheme') ?> &copy; <?php echo date("Y"); ?> &mdash;
+            <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>. <?php _e('All Rights Reserved', 'nadtheme') ?>
+        </div>
+
+        <?php if (has_nav_menu('footer')): ?>
+            <?php wp_nav_menu(array(
+                'theme_location' => 'footer',
+                'menu_id' => 'footer-menu',
+                'menu_class' => 'footer-menu',
+            )); ?>
+        <?php endif; ?>
+    </div>
+
+    <nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e('Footer Social Links Menu', 'elegance'); ?>">
+        <?php if (has_nav_menu('social')): ?>
+            <?php wp_nav_menu(array(
+                'theme_location' => 'social',
+                'menu_id' => 'social-menu',
+                'menu_class' => 'social-menu',
+                'depth' => 1,
+                'link_before' => '<span class="screen-reader-text">',
+                'link_after' => '</span>',
+            )); ?>
+        <?php endif; ?>
+    </nav>
+
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
