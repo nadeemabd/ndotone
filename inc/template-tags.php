4ed29ac457618e19d926033a6228d55840ff91cd
Nadeem Abdulla <nadeemabd@gmail.com>
2016-06-10 16:13:12 +0530

    Change markup of catogeries

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -82,7 +82,7 @@ function nadtheme_entry_footer() {
 	// Hide category and tag text for pages.
 	if ( 'post' === get_post_type() ) {
 		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'nadtheme' ) );
+		$categories_list = get_the_category_list( esc_html__( ' ', 'nadtheme' ) );
 		if ( $categories_list && nadtheme_categorized_blog() ) {
 			printf( '<span class="cat-links">' . esc_html__( '%1$s', 'nadtheme' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 		}
