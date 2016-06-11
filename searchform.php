54abf7cb16533fa8f51b3c9fdec14f74a1e50c0f
Nadeem Abdulla <nadeemabd@gmail.com>
2016-06-11 16:27:49 +0530

    Custom search form

---
+++ searchform.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template for displaying search forms in nadtheme
+ *
+ * @package WordPress
+ * @subpackage nadtheme
+ * @since nadtheme 1.0
+ */
+?>
+
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label>
+		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'nadtheme' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'nadtheme' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+	</label>
+	<button type="submit" class="search-submit"><i class="fa fa-search"></i><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'nadtheme' ); ?></span></button>
+</form>
